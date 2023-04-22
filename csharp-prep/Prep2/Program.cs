dcc13a9fbf67eb169db13f8c467de0496bf202a4
casnoe17 <casnoe17@gmail.com>
2023-04-21 23:45:11 -0600

    modified:   csharp-prep/Prep1/Program.cs 	modified:   csharp-prep/Prep2/Program.cs 	modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade Percentage you earned? ");
+        string grade = Console.ReadLine();
+        int gradePercent = int.Parse(grade);
+        string letter = "";
+        string plusOrMinus = "";
+
+        if (gradePercent >= 90)
+            {
+                letter = "A";
+            }
+        else if (gradePercent >= 80 && gradePercent <= 89)
+            {
+                letter = "B";
+            }
+        else if (gradePercent >= 70 && gradePercent <= 79)
+            {
+                letter = "C";
+            }
+        else if (gradePercent >= 60 && gradePercent <=69)
+            {
+                letter = "D";
+            }
+        else
+            {
+                letter = "F";
+            }
+
+        if (gradePercent < 97)
+            {
+                if (gradePercent % 10 >= 7)
+                    {
+                        plusOrMinus = "+";
+                    }
+                else if (gradePercent % 10 <= 3)
+                    {
+                        plusOrMinus = "-";
+                    }
+            }
+        if (gradePercent <= 50)
+            {
+                plusOrMinus = "";
+            }
+        
+        Console.WriteLine($"Your Letter grade is {letter}{plusOrMinus}.");
+        
+        if (gradePercent >= 70)
+            {
+                Console.WriteLine("You Passed!");
+            }
+        
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string again = "yes";
+                int counter = 0;
+
+        while (again == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0,100);
+            int guess = -1;
+
+            do
+                {
+                    Console.Write("What is your guess? ");
+                    string numGuess = Console.ReadLine();
+                    guess = int.Parse(numGuess);
+                    if (guess < number)
+                        {
+                            Console.WriteLine("Higher");
+                        }
+                    else if (guess > number)
+                        {
+                            Console.WriteLine("Lower");
+                        }
+                    else
+                        {
+                            Console.WriteLine("You chose Correct!");
+                        }
+                    counter += 1;
+                } while (guess != number);
+
+            Console.WriteLine($"You took {counter} tries.");
+            Console.Write("Would you like to play again? ");
+            again = Console.ReadLine();
+        }
+
     }
 }
