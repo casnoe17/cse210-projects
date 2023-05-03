8574d6681ac6be669b204facb79342566e1ba72a
casnoe17 <casnoe17@gmail.com>
2023-05-03 14:56:06 -0600

    new file:   prepare/Learning02/Job.cs 	modified:   prepare/Learning02/Program.cs 	new file:   prepare/Learning02/Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job jobInfo1 = new Job();
+        jobInfo1._company = "Microsoft";
+        jobInfo1._jobTitle = "Software Engineer";
+        jobInfo1._startYear = 2019;
+        jobInfo1._endYear = 2022;
+
+        Job jobInfo2 = new Job();
+        jobInfo2._company = "Apple";
+        jobInfo2._jobTitle = "Manager";
+        jobInfo2._startYear = 2022;
+        jobInfo2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(jobInfo1);
+        myResume._jobs.Add(jobInfo2);
+
+        myResume.displayResume();
     }
 }
